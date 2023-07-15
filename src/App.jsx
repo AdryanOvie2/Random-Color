7bc624360a3cb26eeb068555fc272bb1ce406b57
AdryanOvie2 <adryanovie2@gmail.com>
2023-07-16 01:49:33 +0200

    add styles app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-        
+        <div id='container' className='box'>
+            <p id="color">#2B807B</p>
+            <button id="btn-color">Cambiar Color</button>
+        </div>
     </>
   )
 }
